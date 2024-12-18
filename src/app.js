ba5cb1e2ddfde4dfe1d5b335b86fd0be0ffbb34a
saquibe <saquibe143@gmail.com>
2024-12-18 20:07:51 +0530

    explore req and res

--- src/app.js
+++ src/app.js
@@ -4,24 +4,15 @@ import express from "express";
 const app = express();
 
 //this will only handle GET call request
-app.get("/test", (req, res) => {
+app.get("/test/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ FirstName: "Mohammad", LastName: "Saquib" });
 });
 
-//post call sending data to database
-app.post("/user", (req, res) => {
-  res.send("Data saved to the database successfully!");
-});
-
-//delete call
-app.delete("/user1", (req, res) => {
-  res.send("User 1 is deleted successfully!");
-});
-
 //this will match all the http request
-app.use("/date", (req, res) => {
-  res.send("Updated content: " + new Date());
-});
+// app.use("/date", (req, res) => {
+//   res.send("Updated content: " + new Date());
+// });
 
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000...`);
