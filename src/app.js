8d957ca373d5226e2d90b8d7d0aa7d15a28f8323
saquibe <saquibe143@gmail.com>
2024-12-18 21:19:28 +0530

    this also work same as multiple route handler

--- src/app.js
+++ src/app.js
@@ -3,44 +3,17 @@ import express from "express";
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("First response has send!");
-    // res.send("First response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Second response has send!");
-    // res.send("Second response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Third response has send!");
-    // res.send("Third response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Fourth response has send!");
-    // res.send("Fourth response");
-    next();
-  },
-  (req, res) => {
-    console.log("Fifth response has send!");
-    res.send("Fifth response");
-  }
-);
+app.use("/user", (req, res, next) => {
+  console.log("First response has send!");
+  // res.send("First response");
+  next();
+});
 
-//this will only handle GET call request
-// app.get("/test/:userId/:name/:password", (req, res) => {
-//   console.log(req.params);
-//   res.send({ FirstName: "Mohammad", LastName: "Saquib" });
-// });
-
-//this will match all the http request
-// app.use("/date", (req, res) => {
-//   res.send("Updated content: " + new Date());
-// });
+app.use("/user", (req, res, next) => {
+  console.log("Second response has send!");
+  res.send("Second response");
+  next();
+});
 
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000...`);
