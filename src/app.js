b641669051a4f315a88be13e038b8990ba0c40f2
saquibe <saquibe143@gmail.com>
2024-12-21 21:57:51 +0530

    tested update, delete, patch api

--- src/app.js
+++ src/app.js
@@ -59,6 +59,29 @@ app.get('/feed', async (req, res) => {
 })
 
 
+//delete user
+app.delete('/user', async (req, res) => {
+  const userId = req.body._id; 
+  try {
+    const user = await User.findByIdAndDelete({_id: userId});
+    res.send('User deleted successfully!');
+  } catch (err) {
+    res.status(400).send('Something went wrong!');
+  }
+})
+
+//update data
+app.patch('/user', async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, { new: true });
+    res.send('User updated successfully!');
+  } catch (err) {
+    res.status(400).send('something went wrong');
+  }
+})
+
 connectDb().then(()=> {
   console.log('mongodb connected successfully!');
   app.listen(3000, () => {
