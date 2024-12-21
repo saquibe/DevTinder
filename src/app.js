f0adf6448f804b14b9915ddca70c71bc5e0f2c1b
saquibe <saquibe143@gmail.com>
2024-12-21 20:55:41 +0530

    find specific user and find all user get response from mongodb

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 //middleware
 app.use(express.json());
 
+//signup api
 app.post('/signup', async  (req, res) => {
   // console.log(req.body);
   
@@ -20,6 +21,43 @@ app.post('/signup', async  (req, res) => {
     res.status(400).send('Error saving the user!', err.message);
   }
 })
+
+
+//find user specific user
+// Get user by last name
+app.get('/specific', async (req, res) => {
+  const userLastName = req.query.lastName; // Use req.query for query parameters in GET requests
+  console.log('LastName:', userLastName); // Debugging
+
+  try {
+    if (!userLastName) {
+      return res.status(400).send('LastName is required');
+    }
+
+    const user = await User.findOne({ lastName: userLastName });
+    if (!user) {
+      return res.status(404).send('User not found!');
+    }
+
+    res.send(user);
+  } catch (err) {
+    console.error('Error:', err.message); // Log the error
+    res.status(500).send('Something went wrong');
+  }
+});
+
+
+
+//feed api: get all user from database
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send('Something went wrong!', err.message);
+  }
+})
+
 
 connectDb().then(()=> {
   console.log('mongodb connected successfully!');
