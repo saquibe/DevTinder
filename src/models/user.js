5483a5fdeb197bfb11f63712b2d9890d2b28c315
saquibe <saquibe143@gmail.com>
2024-12-22 10:42:18 +0530

    explore schema checks

--- src/models/user.js
+++ src/models/user.js
@@ -3,24 +3,40 @@ const {Schema} = mongoose;
 
 const userSchema = new Schema({
     firstName:{
-        type: String
+        type: String,
+        required: true
     },
     lastName:{
         type: String
     },
     emailId:{
-        type: String
+        type: String,
+        required: true,
+        unique: true
     },
     password:{
-        type: String
+        type: String,
+        required: true
     },
     age:{
         type: Number
     },
     gender:{
-        type: String
+        type: String,
+        required: true
+    },
+    avatar:{
+        type: String, 
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDXHyqEEcIEQzggUF5RIBe8g37M9n1guqKhg&s"
+    },
+    description: {
+        type: String,
+        default: 'This is default description'
+    },
+    skills: {
+        type: [String]  
     }
-})
+}, {timestamps: true})
 
 const User = mongoose.model("User", userSchema);
 
