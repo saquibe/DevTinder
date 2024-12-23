5c5e920b868c95e06630af2d9c53492f62bbb640
saquibe <saquibe143@gmail.com>
2024-12-23 10:35:05 +0530

    setup for .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDb = async () => {
-  await mongoose.connect('mongodb+srv://saquibe143:OvxWXOiZvC9y0m9F@namastenodejs.cldid.mongodb.net/devTinder')
+  const mongoUri = process.env.MONGO_URI;
+  if (!mongoUri) {
+    throw new Error("MONGO_URI is not defined in the environment variables");
+  }
+  await mongoose.connect(mongoUri);
 }
 
 export default connectDb;
