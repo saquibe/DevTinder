7b6700cd063c96bfafcc8bcffd674a16df6a4f76
saquibe <saquibe143@gmail.com>
2024-12-24 10:06:52 +0530

    add userAuth middleware and expire time to jwt and cookie

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import bcryptjs from 'bcryptjs';
 import cookieParser from "cookie-parser";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { userAuth } from "./middlewares/auth.js";
 
 dotenv.config();
 
@@ -48,9 +49,9 @@ app.post('/login', async (req, res) => {
     const isPasswordValid = await bcryptjs.compare(password, user.password);
     if(isPasswordValid){
       //create jwt token
-      const token = await jwt.sign({_id: user._id},  process.env.JWT_SECRET);
+      const token = await jwt.sign({_id: user._id},  process.env.JWT_SECRET, {expiresIn: "1d"});
       //add the token to cookie and send the response back to the user
-      res.cookie("token", token);
+      res.cookie("token", token, {expires: new Date(Date.now() + 8 * 3600000)});
       res.send('Login successfully!');
     }else{
       throw new Error('Invalid credientials!');
@@ -61,84 +62,20 @@ app.post('/login', async (req, res) => {
 })
 
 //profile api
-app.get('/profile', async (req, res) => {
+app.get('/profile', userAuth, async (req, res) => {
   try {
-    const userCookie = req.cookies;
-    const {token} = userCookie;
-    if(!token){
-      throw new Error('Invalid token!');
-    }
-    //validate token
-    const decodeMessage = await jwt.verify(token, process.env.JWT_SECRET);
-    const {_id} = decodeMessage;
-    //get back profile of user when loggedIn
-    const user = await User.findById(_id);
-    if(!user){
-      throw new Error('User does not exist!');
-    }
+    const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send('Error: ', err.message);
   }
 })
 
-//find user specific user
-// Get user by last name
-app.get('/specific', async (req, res) => {
-  const userLastName = req.query.lastName; // Use req.query for query parameters in GET requests
-  console.log('LastName:', userLastName); // Debugging
-
-  try {
-    if (!userLastName) {
-      return res.status(400).send('LastName is required');
-    }
-
-    const user = await User.findOne({ lastName: userLastName });
-    if (!user) {
-      return res.status(404).send('User not found!');
-    }
-
-    res.send(user);
-  } catch (err) {
-    console.error('Error:', err.message); // Log the error
-    res.status(500).send('Something went wrong');
-  }
-});
-
-
-
-//feed api: get all user from database
-app.get('/feed', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(400).send('Error: ', err.message);
-  }
-})
-
-
-//delete user
-app.delete('/user', async (req, res) => {
-  const userId = req.body._id; 
-  try {
-    const user = await User.findByIdAndDelete({_id: userId});
-    res.send('User deleted successfully!');
-  } catch (err) {
-    res.status(400).send('Something went wrong!');
-  }
-})
-
-//update data
-app.patch('/user', async (req, res) => {
-  const userId = req.body.userId;
-  const data = req.body;
-  try {
-    const user = await User.findByIdAndUpdate(userId, data, { new: true });
-    res.send('User updated successfully!');
-  } catch (err) {
-    res.status(400).send('something went wrong');
-  }
+//sending connection request api
+app.post('/connection', userAuth, async (req, res) => {
+  const user = req.user;
+  console.log('Sending connection request!');
+  res.send(user.lastName+' send the connection request');
 })
 
 connectDb().then(()=> {
