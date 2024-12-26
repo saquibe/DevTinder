065ffe736a9cdf7e9925e385b0bee5d806be11f3
saquibe <saquibe143@gmail.com>
2024-12-26 11:20:47 +0530

    add connection fetching route and update connection request model to reference User

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,4 +15,23 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
   }
 })
 
+//get all connections of the logged in user
+userRouter.get('/user/connections', userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequestModel.find({ $or: [{ fromUserId: loggedInUser._id, status: 'accepted' }, { toUserId: loggedInUser._id, status: 'accepted' }]}).populate('fromUserId', ['firstName', 'lastName', 'avatar', 'age', 'gender', 'skills', 'description']).populate('toUserId', ['firstName', 'lastName', 'avatar', 'age', 'gener', 'skills', 'description']);
+
+    const data = connectionRequests.map((row) => {
+      if(row.fromUserId._id.equals(loggedInUser._id)){
+        return row.toUserId;
+      }else{
+        return row.fromUserId
+    }});
+
+    res.json({message: 'Connections fetched successfully!', data});
+  } catch (err) {
+    res.statusCode(400).send('Error: '+ err.message);
+  }
+})
+
 export default userRouter;
